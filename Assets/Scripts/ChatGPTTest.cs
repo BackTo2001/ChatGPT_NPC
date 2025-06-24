2fb8341bf54fa89d083f5976f04777e48f788913
김나현 <skgus917@gmail.com>
2025-06-24 12:44:30 +0900

    이전 대화 기록하도록 수정

--- Assets/Scripts/ChatGPTTest.cs
+++ Assets/Scripts/ChatGPTTest.cs
@@ -15,6 +15,8 @@ public class ChatGPTTest : MonoBehaviour
     private const string OPEANAI_API_KEY =
         "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";
 
+    private List<Message> _messages = new List<Message>();
+
     public async void Send()
     {
         // 0. 프롬프트(=AI에게 원하는 명령을 적은 텍스트)를 읽어온다.
@@ -25,18 +27,15 @@ public class ChatGPTTest : MonoBehaviour
         }
         PromptField.text = string.Empty;
 
-
         // 1. API 클라이언트 초기화 -> ChatGPT 접속
         var api = new OpenAIClient(OPEANAI_API_KEY);
 
-        // 2. 메시지 작성
-        var messages = new List<Message>
-        {
-            new Message(Role.User, prompt),
-        };
+        // 2. 메시지 작성 후 메시지's 리스트에 추가
+        Message promptMessage = new Message(Role.User, prompt);
+        _messages.Add(promptMessage);
 
         // 3. 메시지 보내기
-        var chatRequest = new ChatRequest(messages, Model.GPT4o);
+        var chatRequest = new ChatRequest(_messages, Model.GPT4o);
 
         // 4. 답변 받기
         var response = await api.ChatEndpoint.GetCompletionAsync(chatRequest);
@@ -47,6 +46,10 @@ public class ChatGPTTest : MonoBehaviour
         // 6. 답변 출력
         Debug.Log($"[{choice.Index}] {choice.Message.Role}: {choice.Message} | Finish Reason: {choice.FinishReason}");
         ResultTextUI.text = choice.Message;
+
+        // 7. 답변도 message's 추가
+        Message resultMessage = new Message(Role.Assistant, prompt);
+        _messages.Add(resultMessage);
     }
 
 }
